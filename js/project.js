ea08ba4b33c211b7cf9ec66938b067ab71c3cae1
Muhammad Yizimu <64603940+MuhammadYildiz@users.noreply.github.com>
2022-11-26 09:38:09 +0100

    Update project.js

--- js/project.js
+++ js/project.js
@@ -30,7 +30,7 @@ let number = 0;
     projectPic.innerHTML =`<img class="" src="${dataMain[number].Pictury}" alt="">`
     projectDescrip.innerHTML = `${dataMain[number].description}
     <img src="https://img.freepik.com/free-vector/laptop-with-program-code-isometric-icon-software-development-programming-applications-dark-neon_39422-971.jpg?w=826&t=st=1668779826~exp=1668780426~hmac=8a281b4e153303ff29c87055fc53d8f8e96d90cc67ba79675d35e21fece6d002" alt="">  `
-    projectlink.innerHTML  =`Click on the link to access the project: <a href = '${dataMain[number].projectURL}' target="_blank">${dataMain[number].projectNmne }</a> `
+    projectlink.innerHTML  =`Click on the link to access the project on github repository: <a href = '${dataMain[number].projectURL}' target="_blank">${dataMain[number].projectNmne }</a> `
   })
 
 })
